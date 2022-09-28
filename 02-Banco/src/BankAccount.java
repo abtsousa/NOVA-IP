0fa9c46ff7b7f82ae36741de03f4dbc17a0d2966
afonso <afonso@Serena.lan>
2022-09-28 19:29:56 +0100

    bank

---
+++ 02-Banco/src/BankAccount.java
@@ -0,0 +1,14 @@
+
+public class BankAccount {
+	private int balance;
+	public BankAccount() {balance=0;}
+	/* Pre: initial >=0 */
+	public BankAccount(int initial) {balance = initial;}
+	
+	/* Pre: amount > 0 */
+	public void deposit (int amount) {balance = balance + amount;}
+	public void withdraw (int amount) {balance = balance - amount;}
+	
+	public int getBalance() {return balance;}
+	public boolean isInRedZone () {return balance < 0;}
+}
