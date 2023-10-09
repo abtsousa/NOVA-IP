5fb6fc44ffc434f4b45aa0e2e3f8d1314791ba97
abtsousa <11749310+abtsousa@users.noreply.github.com>
2023-10-09 01:50:34 +0100

    file organizing

--- 00-HelloWorldProject/src/Lamp.java
+++
@@ -1,8 +0,0 @@
-
-public class Lamp {
-	private boolean lampIsOn;
-	public Lamp() { lampIsOn = false; }
-	public void turnOn() { lampIsOn = true; }
-	public void turnOff() { lampIsOn = false; }
-	public boolean isOn() { return lampIsOn; }
-}
